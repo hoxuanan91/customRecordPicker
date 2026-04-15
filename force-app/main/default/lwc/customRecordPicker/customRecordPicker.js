858916b4b2120f15814d5ac9a9fe0438d41b37c0
anho91 <hxa.ql.vn@gmail.com>
2026-04-15 13:55:07 +0200

    Remove post-processing and dataType logic for pure SOSL WHERE filtering
    - Remove all post-search filtering logic (applyFilters, evaluateFilters, etc.)
    - Remove dataType from FilterCriterion class and all related logic
    - Force all filter criteria into SOSL WHERE clause with error handling
    - Update all tests to remove dataType references
    - Simplify formatSoslValue and formatSoslInValue methods
    - Add try-catch in search method to catch SOSL syntax errors
    - Streamline buildSoslCondition to always generate conditions

--- force-app/main/default/lwc/customRecordPicker/customRecordPicker.js
+++ force-app/main/default/lwc/customRecordPicker/customRecordPicker.js
@@ -365,15 +365,15 @@ export default class CustomRecordPicker extends OmniscriptBaseMixin(
     "iconName": "standard:account",
     "filter": {
         "criteria": [
-            { "fieldPath": "Type", "operator": "eq", "value": "Prospect", "dataType": "Picklist" },
-            { "fieldPath": "Type", "operator": "ne", "value": "Other", "dataType": "Picklist" },
+            { "fieldPath": "Type", "operator": "eq", "value": "Prospect" },
+            { "fieldPath": "Type", "operator": "ne", "value": "Other" },
             { "fieldPath": "Name", "operator": "like", "value": "%a%" },
-            { "fieldPath": "CreatedDate", "operator": "gt", "value": { "literal": "LAST_N_YEARS:5" }, "dataType": "DateTime" },
-            { "fieldPath": "CreatedDate", "operator": "gte", "value": { "literal": "LAST_N_DAYS:365" }, "dataType": "DateTime" },
-            { "fieldPath": "CreatedDate", "operator": "lt", "value": { "literal": "TOMORROW" }, "dataType": "DateTime" },
-            { "fieldPath": "CreatedDate", "operator": "lte", "value": { "literal": "TODAY" }, "dataType": "DateTime" },
-            { "fieldPath": "Type", "operator": "in", "value": ["Prospect", "Customer"], "dataType": "[Picklist]" },
-            { "fieldPath": "Type", "operator": "nin", "value": ["Other"], "dataType": "[Picklist]" }
+            { "fieldPath": "CreatedDate", "operator": "gt", "value": { "literal": "LAST_N_YEARS:5" } },
+            { "fieldPath": "CreatedDate", "operator": "gte", "value": { "literal": "LAST_N_DAYS:365" } },
+            { "fieldPath": "CreatedDate", "operator": "lt", "value": { "literal": "TOMORROW" } },
+            { "fieldPath": "CreatedDate", "operator": "lte", "value": { "literal": "TODAY" } },
+            { "fieldPath": "Type", "operator": "in", "value": ["Prospect", "Customer"] },
+            { "fieldPath": "Type", "operator": "nin", "value": ["Other"] }
         ],
         "filterLogic": "1 OR 2 OR 3 OR 4 OR 5 OR 6 OR 7 OR 8 OR 9"
     },
